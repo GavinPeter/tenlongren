a031e9e7db1527530ccde6747d57acc8f78f58fe
goooooooogle <kangxilord@gmail.com>
2014-09-03 20:51:02 +0800

    update

--- tenlongrener-for-bookmarklet.js
+++ tenlongrener-for-bookmarklet.js
@@ -650,10 +650,7 @@ function detectNewImage(src, async, target) {
 }
 
 function tenlongrenerEvents() {
-  $('body').append('<canvas id="tenlongren_output" style="display:none;"></canvas>
-  <img id="helmet_layer" src="http://goooooooogle.github.io/tenlongren/helmet_layer.png" style="display:none;">
-  <img id="helmet_layer_2x" src="http://goooooooogle.github.io/tenlongren/helmet_layer_2x.png" style="display:none;">
-  <img id="helmet_layer_4x" src="http://goooooooogle.github.io/tenlongren/helmet_layer_4x.png" style="display:none;">');
+  $('body').append('<canvas id="tenlongren_output" style="display:none;"></canvas><img id="helmet_layer" src="http://goooooooogle.github.io/tenlongren/helmet_layer.png" style="display:none;"><img id="helmet_layer_2x" src="http://goooooooogle.github.io/tenlongren/helmet_layer_2x.png" style="display:none;"><img id="helmet_layer_4x" src="http://goooooooogle.github.io/tenlongren/helmet_layer_4x.png" style="display:none;">');
 }
 
 if (!($ = window.jQuery)) {
